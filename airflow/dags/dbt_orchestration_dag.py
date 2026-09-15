42edd9d92d1a82f3f83182ebdda28d430a238598
jannabaah-code <janna.baah@gmail.com>
2026-09-15 14:49:53 -0400

    Add files via upload

---
+++ airflow/dags/dbt_orchestration_dag.py
@@ -0,0 +1,28 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from airflow.utils.dates import days_ago
+
+default_args = {
+    "owner": "janna",
+    "retries": 1
+}
+
+with DAG(
+    dag_id="dbt_orchestration",
+    default_args=default_args,
+    schedule_interval="0 */6 * * *",  # every 6 hours
+    start_date=days_ago(1),
+    catchup=False
+) as dag:
+
+    dbt_run = BashOperator(
+        task_id="dbt_run",
+        bash_command="cd /usr/local/airflow/dbt && dbt run"
+    )
+
+    dbt_test = BashOperator(
+        task_id="dbt_test",
+        bash_command="cd /usr/local/airflow/dbt && dbt test"
+    )
+
+    dbt_run >> dbt_test
